2bc8a47b19f86babc72b944e6a9bc2f73b2ec24f
k12e <kalyanirathod2095@gmail.com>
2022-05-31 16:41:40 +0530

    Added: store the daily total wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,35 +1,40 @@
 console.log("Welcome to Employee payroll program");
 
-//UC5 :Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
+//UC6 :Store the Daily Wage along with the Total Wage - Save in an Array the Daily Wage
 
 const IS_PART_TIME = 1;  
 const IS_FULL_TIME = 2; 
 const PART_TIME_HOURS = 4; 
 const FULL_TIME_HOURS = 8; 
-const WAGE_PER_HOUR = 20; 
-const NUM_OF_WORKING_DAYS = 20;
-const MAX_HRS_IN_MONTH = 160;
-
 function getWorkingHours (empCheck)
- {
-     switch(empCheck)
+{
+ switch(empCheck)
 {
     case IS_PART_TIME:
     return PART_TIME_HOURS;
     case IS_FULL_TIME:
-     return FULL_TIME_HOURS;
+    return FULL_TIME_HOURS;
     default:
-     return 0;
+    return 0;
 }
- }
-let empHours = 0;
+}
+
+const WAGE_PER_HOUR = 20; 
+const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+let totalEmpHrs = 0;
 let totalWorkingDays = 0;
-let totalWorkingHours = 0;
-while (totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) 
-{
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random()*10)%3;
-    totalWorkingHours += getWorkingHours(empCheck);
+let empDailyWageArr = new Array();
+function calcDailyWage(empHrs){
+    return empHrs*WAGE_PER_HOUR;
 }
-let empWage = totalWorkingHours * WAGE_PER_HOUR;
-console.log("Total Days: "+totalWorkingDays +" Total Hours: "+totalWorkingHours+ " Employee wage: "+ empWage);
+
+while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs+=empHrs;
+    empDailyWageArr.push(calcDailyWage(empHrs));
+}
+let empWage = calcDailyWage(totalEmpHrs);
+console.log("Total Days: "+totalWorkingDays + ", Total Hours: "+totalEmpHrs+ ", Employee Wage: "+empWage)
