1f8861bb1a47bf5c9b710fc93d4671ad26cfe40f
k12e <kalyanirathod2095@gmail.com>
2022-06-03 14:08:19 +0530

    Added: extends the employee payroll class to date

--- EmployeePayRoll.js
+++ EmployeePayRoll.js
@@ -6,10 +6,12 @@
      startDate;
 
      //constructor
-     constructor(id, name,salary){
-         this.id=id;
-         this.name=name;
-         this.salary=salary;
+     constructor(...params){
+         this.id=params[0];
+         this.name=params[1];
+         this.salary=params[2];
+         this.gender=params[3];
+         this.startDate=params[4];
      }
      //getter setter method
      get name()
@@ -22,7 +24,10 @@
      }
      //method
      toString(){
-        return "id=" + this.id +", name='" +this.name +", salary=" +this.salary; 
+         const options={year:'numeric',month:'long',day:'numeric'};
+         const empdate=this.startDate===undefined ? "undefined":
+                        this.startDate.toLocalDateString("en-US",options);
+        return "id=" + this.id +", name='" +this.name +", salary=" +this.salary +", "+ "gender=" +this.gender + ", startDate=" +empdate;
      }
     }
     let employeePayRollData= new EmployeePayRollData(1,"Kalyani",20000);
@@ -30,4 +35,6 @@
     employeePayRollData.id=2;
     employeePayRollData.name="Ayansh";
     employeePayRollData.salary=30000;
+    console.log(employeePayRollData.toString());
+    let employeePayRollData1=new EmployeePayRollData(1,"Swati",30000,"F",new Date());
     console.log(employeePayRollData.toString());
